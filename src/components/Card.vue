0dbfc612329283c2d28860faaa3eb87251c46180
personal <shaneel_kumar@live.com>
2023-04-20 01:22:41 +1200

    add card component with basic styling

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  background: var(--card-bg);
+  aspect-ratio: 1/1.5;
+  border-radius: 1rem;
+  box-shadow: 0px 0px 20px 10px hsla(0, 0, 0, 0.1);
+}
+</style>
