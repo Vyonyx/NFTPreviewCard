b2dea8145ab8f93bf06b08b5cf96761d14b346e7
personal <shaneel_kumar@live.com>
2023-04-20 01:08:29 +1200

    add background color

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
 
 <template>
   <main>
-    <h1>Hello World</h1>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  background: var(--main-bg);
+  height: 100vh;
+}
+</style>
 
