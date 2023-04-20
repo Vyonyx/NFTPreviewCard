8a954b1ae86f8f15edcebbcebad26fcc12dd7961
personal <shaneel_kumar@live.com>
2023-04-21 10:00:49 +1200

    add artwork image to Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,16 +3,34 @@
 
 <template>
   <div class="card">
+    <div class="artwork">
+      <img src="../assets/image-equilibrium.jpg" alt="nft artwork">
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .card {
   width: 100%;
-  max-width: 350px;
+  max-width: 320px;
   background: var(--card-bg);
-  aspect-ratio: 1/1.5;
+  aspect-ratio: 1/1.7;
   border-radius: 1rem;
   box-shadow: 0px 0px 20px 10px hsla(0, 0, 0, 0.1);
+  padding: 1.5rem;
+}
+
+.artwork {
+  width: 100%;
+  aspect-ratio: 1/1;
+  border-radius: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+  }
 }
 </style>
