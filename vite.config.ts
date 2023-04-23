a9cde351fb52b20557bc8a72c1283e922cbc3745
personal <shaneel_kumar@live.com>
2023-04-23 21:51:36 +1200

    update base url and deployment files to suit repository name change on GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NFTPreviewCard/',
+  base: '/NFTPreviewCard-feMentors-Vue/',
   plugins: [vue()],
 })
